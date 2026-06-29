4816f4471e8e474126142c4ae8b128f13575539a
thearvindas <thearvindas@gmail.com>
2026-06-28 20:09:32 -0600

    finished chap 2 exercises

---
+++ chapter2/q5/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int x;
+    printf("Enter x: ");
+    scanf("%d", &x);
+
+    printf("%d\n", 3 * x * x * x * x * x 
+                    + 2 * x * x * x * x
+                    - 5 * x * x * x
+                    - x * x
+                    + 7 * x
+                    - 6);
+
+    return 0;
+
+}
